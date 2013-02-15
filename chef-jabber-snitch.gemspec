88f7206a8aa8a9000b895477dc9225bbacd66703
Kendrick Martin <kendrick.martin@webtrends.com>
2013-02-15 14:00:13 -0800

    Adding support for pastebin and jabber rooms

--- chef-jabber-snitch.gemspec
+++ chef-jabber-snitch.gemspec
@@ -1,19 +1,16 @@
 Gem::Specification.new do |s|
   s.name        = "chef-jabber-snitch"
-  s.version     = "0.0.4"
+  s.version     = "1.0.0"
   s.platform    = Gem::Platform::RUBY
-  s.authors     = ["Alice Kaerast"]
-  s.email       = ["alice.kaerast@webanywhere.co.uk"]
+  s.authors     = ["Kendrick Martin"]
+  s.email       = ["kendrick.martin@webtrends.com"]
   s.homepage    = "https://github.com/kaerast/chef-jabber-snitch"
-  s.summary     = %q{An exception handler for OpsCode Chef runs (GitHub Gists & Jabber)}
-  s.description = %q{An exception handler for OpsCode Chef runs (GitHub Gists & Jabber)}
+  s.summary     = %q{An exception handler for OpsCode Chef runs (Pastebin & Jabber)}
+  s.description = %q{An exception handler for OpsCode Chef runs (Pastebin & Jabber)}
   s.has_rdoc    = false
   s.license     = "MIT"
 
-  #s.rubyforge_project = "chef-irc-snitch"
-
   s.add_dependency('chef')
-  #s.add_dependency('carrier-pigeon')
   s.add_dependency('xmpp4r')
 
   s.files         = `git ls-files`.split("\n")
--- lib/chef-jabber-snitch.rb
+++ lib/chef-jabber-snitch.rb
@@ -5,16 +5,20 @@ require 'net/http'
 require 'uri'
 require 'json'
 require 'xmpp4r/client'
+require 'xmpp4r'
+require 'xmpp4r/muc'
+require 'xmpp4r/roster'
 
 class JabberSnitch < Chef::Handler
 
-  def initialize(jabber_user, jabber_password, jabber_to, github_user, github_token, jabber_server = 'talk.google.com')
+  def initialize(jabber_user, jabber_password, jabber_room, pastebin_server, jabber_server = 'talk.google.com', pastebin_expire = 74880, jabber_port = 5222)
     @jabber_user = jabber_user
     @jabber_password = jabber_password
     @jabber_server = jabber_server
-    @jabber_to = jabber_to
-    @github_user = github_user
-    @github_token = github_token
+    @jabber_room = jabber_room
+    @pastebin_server = pastebin_server
+    @pastebin_expire = pastebin_expire
+    @jabber_port = jabber_port
     @timestamp = Time.now.getutc
   end
 
@@ -22,61 +26,55 @@ class JabberSnitch < Chef::Handler
     node.run_list.map {|r| r.type == :role ? r.name : r.to_s }.join(', ')
   end
 
-  def fmt_gist
-    ([ "Node: #{node.name} (#{node.ipaddress})",
-       "Run list: #{node.run_list}",
-       "All roles: #{node.roles.join(', ')}",
-       "",
-       "#{run_status.formatted_exception}",
-       ""] +
-     Array(backtrace)).join("\n")
+  def fmt_output
+    message =  "Node: #{node.name} (#{node.ipaddress})\n"
+    message << "Run list: #{node.run_list}\n"
+    message << "All roles: #{node.roles.join(', ')}\n"
+    message << "\n"
+    message << "#{run_status.formatted_exception}\n"
+    message << Array(backtrace).join("\n")
+    return message
   end
 
   def report
-
+    Chef::Log.debug("Starting jabber handling")
     if STDOUT.tty?
       Chef::Log.error("Chef run failed @ #{@timestamp}")
       Chef::Log.error("#{run_status.formatted_exception}")
     else
       Chef::Log.error("Chef run failed @ #{@timestamp}, snitchin' to chefs via Jabber")
-
-      gist_id = nil
+    end     
+    res = nil
       begin
-        timeout(10) do
-          res = Net::HTTP.post_form(URI.parse("http://gist.github.com/api/v1/json/new"), {
-            "files[#{node.name}-#{@timestamp.to_i.to_s}]" => fmt_gist,
-            "login" => @github_user,
-            "token" => @github_token,
-            "description" => "Chef run failed on #{node.name} @ #{@timestamp}",
-            "public" => false
-          })
-          gist_id = JSON.parse(res.body)["gists"].first["repo"]
-          Chef::Log.info("Created a GitHub Gist @ https://gist.github.com/#{gist_id}")
+         timeout(10) do
+          res = Net::HTTP.post_form(URI.parse(@pastebin_server), {"expire" => @pastebin_expire, "text" => fmt_output, "name" => node.name, "title" => "Failed Chef run #{@timestamp}"})
+          Chef::Log.info("Created output #{res.body}")
         end
       rescue Timeout::Error
-        Chef::Log.error("Timed out while attempting to create a GitHub Gist")
+        Chef::Log.error("Timed out while attempting to post to pastebin server #{pastebin_server}")
       end
 
-      message = "Chef failed on #{node.name} (#{fmt_run_list}): https://gist.github.com/#{gist_id}"
+      message = "Chef failed on #{node.name} Run List - (#{fmt_run_list}) #{res.body}"
 
       begin
+        Chef::Log.debug("It's jabber time")
         timeout(10) do
 
-          jid = Jabber::JID::new(@jabber_user)
-          cl = Jabber::Client::new(jid)
-          cl.connect(@jabber_server,5222)
-          cl.auth(@jabber_password)
-          to = @jabber_to
-          subject = "Chef failure"
-          m = Jabber::Message::new(to, message)
-          m.type = :chat
-          cl.send m
+          chef_jid = Jabber::JID::new(@jabber_user)
+          chef_client = Jabber::Client::new(chef_jid)
+          chef_client.connect(@jabber_server,@jabber_port)
+          chef_client.auth(@jabber_password)
+
+          muc = Jabber::MUC::MUCClient.new(chef_client)
+          muc.join(Jabber::JID::new("#{@jabber_room}/" + chef_client.jid.node))
+
+          m = Jabber::Message::new(@jabber_room, message)
+          muc.send m
           Chef::Log.info("Informed chefs via Jabber '#{message}'")
         end
       rescue Timeout::Error
         Chef::Log.error("Timed out while attempting to message Chefs via Jabber")
-      end
-    end
+     end
   end
 
 end
